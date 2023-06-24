430600d2ef6b116509fdb20b583f34e8e5563ba6
kiririx <kiriri_ri@126.com>
2023-06-24 12:24:37 +0800

    feat: complete

--- main.go
+++ main.go
@@ -6,32 +6,41 @@ import (
 	"github.com/gorilla/websocket"
 	"log"
 	"net/url"
+	"time"
 )
 
 func main() {
-	host := ""
+	for {
+		_ = connect()
+		time.Sleep(time.Second * 5)
+	}
+}
+
+func connect() error {
+	host := "101.42.239.41:10042"
 	u := url.URL{Scheme: "ws", Host: host, Path: "/ws/receive"}
 	log.Printf("connecting to %s", u.String())
 
 	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
-		log.Fatal("dial:", err)
+		return err
 	}
 	defer c.Close()
 
 	log.Printf("已经连接到%s", host)
 
 	for {
+		// read message
 		_, message, err := c.ReadMessage()
 		if err != nil {
 			log.Printf("读取消息失败, err=%s", err)
-			return
+			return err
 		}
 		log.Printf("received message: %s", message)
 
 		resp := make(map[string]string)
 		_ = json.Unmarshal(message, &resp)
-		err = beeep.Notify("短信", resp["message"], "icon.jpeg")
+		err = beeep.Notify("†✰⇝->♡†", resp["message"], "icon.jpeg")
 		if err != nil {
 			log.Printf("发送系统通知失败, err=" + err.Error())
 		}
